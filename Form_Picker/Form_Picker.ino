87ea6bda161f93dee4dddbacbad808d72dd90ec8
captaindimak <ozette9@gmail.com>
2019-01-14 18:12:52 -0800

    Re-number all buttons and change all following values to work as expected. Final wiring is done!

--- Form_Picker/Form_Picker.ino
+++ Form_Picker/Form_Picker.ino
@@ -6,17 +6,18 @@
 #include <Bounce2.h>
 
 // Set button pins for use later, diagram of how wiring matches in box included.
-#define BUTTON_PIN_1 0
-#define BUTTON_PIN_2 1
-#define BUTTON_PIN_3 2
-#define BUTTON_PIN_4 3
-#define BUTTON_PIN_5 4
-#define BUTTON_PIN_6 5
+
+#define BUTTON_PIN_0 0
+#define BUTTON_PIN_1 1
+#define BUTTON_PIN_2 2
+#define BUTTON_PIN_3 3
+#define BUTTON_PIN_4 4
+#define BUTTON_PIN_5 5
 
 /* Physical button layout reference:
  * ┏━━━━━━━━┓
- * ┃ 1 2 3 ┃
- * ┃ 4 5 6 ┃
+ * ┃ 0 2 4 ┃
+ * ┃ 1 3 5 ┃
  * ┗━━━━━━━━┛
  */
 
@@ -24,12 +25,13 @@
 #define LED_PIN 13
 
 // Instantiate Bounce objects for buttons
+
+Bounce button0 = Bounce();
 Bounce button1 = Bounce();
 Bounce button2 = Bounce();
 Bounce button3 = Bounce();
 Bounce button4 = Bounce();
 Bounce button5 = Bounce();
-Bounce button6 = Bounce();
 
 // Setup press status boolean
 bool pressed = false;
@@ -40,34 +42,28 @@ int someMilliseconds = 20;
 void setup() {
 
   // Setup the buttons with an internal pull-up
+  pinMode(BUTTON_PIN_0, INPUT_PULLUP);
+  button1.attach(BUTTON_PIN_0); // setup the Bounce instance
+  button1.interval(someMilliseconds); // interval in ms
+
   pinMode(BUTTON_PIN_1, INPUT_PULLUP);
-  // After setting up the button, setup the Bounce instance
-  button1.attach(BUTTON_PIN_1);
+  button1.attach(BUTTON_PIN_1); // setup the Bounce instance
   button1.interval(someMilliseconds); // interval in ms
 
   pinMode(BUTTON_PIN_2, INPUT_PULLUP);
-  // After setting up the button, setup the Bounce instance
-  button2.attach(BUTTON_PIN_2);
+  button2.attach(BUTTON_PIN_2); // setup the Bounce instance
   button2.interval(someMilliseconds); // interval in ms
 
   pinMode(BUTTON_PIN_3, INPUT_PULLUP);
-  // After setting up the button, setup the Bounce instance
-  button2.attach(BUTTON_PIN_3);
+  button2.attach(BUTTON_PIN_3); // setup the Bounce instance
   button2.interval(someMilliseconds); // interval in ms
 
   pinMode(BUTTON_PIN_4, INPUT_PULLUP);
-  // After setting up the button, setup the Bounce instance
-  button2.attach(BUTTON_PIN_4);
+  button2.attach(BUTTON_PIN_4); // setup the Bounce instance
   button2.interval(someMilliseconds); // interval in ms
 
   pinMode(BUTTON_PIN_5, INPUT_PULLUP);
-  // After setting up the button, setup the Bounce instance
-  button2.attach(BUTTON_PIN_5);
-  button2.interval(someMilliseconds); // interval in ms
-
-  pinMode(BUTTON_PIN_6, INPUT_PULLUP);
-  // After setting up the button, setup the Bounce instance
-  button2.attach(BUTTON_PIN_6);
+  button2.attach(BUTTON_PIN_5); // setup the Bounce instance
   button2.interval(someMilliseconds); // interval in ms
 
   // Setup the LED:
@@ -77,20 +73,21 @@ void setup() {
 
 void loop() {
   // Update the Bounce instances
+
+  button0.update();
   button1.update();
   button2.update();
   button3.update();
   button4.update();
   button5.update();
-  button6.update();
 
   // Get the updated value
+  int value0 = button0.read();
   int value1 = button1.read();
   int value2 = button2.read();
   int value3 = button2.read();
   int value4 = button2.read();
   int value5 = button2.read();
-  int value6 = button2.read();
 
   // Turn on the LED if any button is pressed
   if (button1.fell() || button2.fell() || button3.fell() || button4.fell() || button5.fell() || button6.fell()) {
@@ -109,4 +106,3 @@ void loop() {
   }
 
 }
-
